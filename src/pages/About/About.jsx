b4f1a11d16343f69dab71150600e682b9e891246
nicolc <nicolas.l.campos1@gmail.com>
2023-12-13 19:20:33 -0300

    css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -66,7 +66,7 @@ export const About = () => {
             <div className="about-img-container">
                 <img src="https://i.ibb.co/r7YXMVJ/Nico.jpg" alt="" className="home-img"  />
             </div>
-            <p id="about-txt" className="about-txt">Hello! My name is Nico, a Frontend developer from Argentina. I am a Computer Science student at UBA (Unviersidad de Buenos Aires) and learning Fullstack development at Coderhouse and I am interested in everything referred to web development. I can also speak Spanish at a native level, and Japanese at an intermediate level. I am currently looking for my first work experience in the IT field, in the meantime I keep learning and making projects.</p>
+            <p id="about-txt" className="about-txt">Hello! My name is Nico, a Frontend developer from Argentina. I am a Computer Science student at UBA (Universidad de Buenos Aires) and learning Fullstack development at Coderhouse and I am interested in everything referred to web development. I can also speak Spanish at a native level, and Japanese at an intermediate level. I am currently looking for my first work experience in the IT field, in the meantime I keep learning and making projects.</p>
         </div>
         <div className="tech-section">
             <p className="tech-i-use">Technologies that I use</p>
