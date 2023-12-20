4d90b0a004f97d3e93bcb412bf7083a6da03e8fe
nicolc <nicolas.l.campos1@gmail.com>
2023-12-20 11:48:39 -0300

    Fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,23 +3,33 @@ import '../../styles/home.css'
 import { HomeBtn } from "../../components/HomeBtn/HomeBtn";
 
 export const Home = () => {
+
+    setTimeout(() => {
+        document.querySelector('.first-set').classList.add('animate__fadeIn')
+        document.querySelector('.first-set').classList.remove('hide')
+        document.querySelector('.second-set').classList.add('animate__fadeIn')
+        document.querySelector('.second-set').classList.remove('hide')
+      }, "1000");
+
     return (
     <>
     <div className="main-container">
         
-        <div className="home-img-container">
+        <div className=" animate__animated animate__fadeIn home-img-container">
             <div className="box__line box__line--top"></div>
             <div className="box__line box__line--right"></div>
             <div className="box__line box__line--bottom"></div>
             <div className="box__line box__line--left"></div>
             <img src="https://i.ibb.co/wwDVPxk/Nico.jpg" alt="" className="home-img" />
         </div>
-        <div className="btn-section">
-            <div className="first-set">
+        <p>Hey! I am Nico</p>
+        <p>A web developer</p>
+        <div className=" btn-section">
+            <div className="hide first-set">
                 <HomeBtn label='Projects'/>
                 <HomeBtn label='About Me'/>
             </div>
-            <div className="second-set">
+            <div className="hide second-set">
                 <HomeBtn label='Contact'/>
                 <HomeBtn label='Extra'/>
             </div>
